329a8c72d911fa0ce094964f904df2b9be0f89ab
noyonali492 <noyonahmed492@gmail.com>
2024-12-07 23:45:13 +0600

    Describe vuld

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
 
 abstract class Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 
 Route::group([
 
-    // 'middleware' => 'api',
-   //'namespace' => 'App\Http\Controllers',
+    'middleware' => 'api',
+   'namespace' => 'App\Http\Controllers',
     'prefix' => 'auth'
 
 ], function ($router) {
 
     Route::post('login', [AuthController::class, 'login']);
+    Route::post('signup', [AuthController::class, 'signup']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
